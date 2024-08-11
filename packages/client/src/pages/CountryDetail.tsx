6ccd4e2829c5a706e8cb2e78e8ea7504d9888110
pedroharbs <harbspj@gmail.com>
2024-08-11 22:19:49 +0200

    feat: implement country details page

--- packages/client/src/pages/CountryDetail.tsx
+++ packages/client/src/pages/CountryDetail.tsx
@@ -1,15 +1,66 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import type { Country } from "schemas";
+import { InternalBox } from "../components/InternalBox";
+import { getCountryById } from "../services/countries/get-country";
 
-const CountryDetail: React.FC = () => {
+export default function CountryDetails() {
   const { id } = useParams<{ id: string }>();
 
-  return (
-    <div>
-      <h1>Country Detail</h1>
-      <p>Country ID: {id}</p>
-    </div>
-  );
-};
+  const [country, setCountry] = useState<Country | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-export default CountryDetail;
+  useEffect(() => {
+    const fetchResults = async () => {
+      if (!id) return;
+
+      try {
+        setLoading(true);
+        const fetchedCountry = await getCountryById(id);
+        setCountry(fetchedCountry);
+      } catch (err) {
+        setError("Failed to fetch country details.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchResults();
+  }, [id]);
+
+  if (loading)
+    return (
+      <InternalBox>
+        <h3 className="text-center">Loading...</h3>
+      </InternalBox>
+    );
+
+  if (error)
+    return (
+      <InternalBox>
+        <p>{error}</p>
+      </InternalBox>
+    );
+
+  if (!country)
+    return (
+      <InternalBox>
+        <h3 className="text-center">No country found</h3>
+      </InternalBox>
+    );
+
+  return (
+    <InternalBox>
+      <h1 className="mb-4">{country.country}</h1>
+      <div className="mb-3">
+        <p>
+          <strong>Country:</strong> {country.country}
+        </p>
+        <p>
+          <strong>ISO Code:</strong> {country.countryIsoCode}
+        </p>
+      </div>
+    </InternalBox>
+  );
+}
