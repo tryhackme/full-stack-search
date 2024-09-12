76cb2f44403acdf951896e8dbe2d23f97d78df0a
lexico4real <lexico4real@gmail.com>
2024-09-12 22:49:42 +0100

    implement debounce

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,16 +1,36 @@
-import { useState, type ChangeEvent } from 'react';
+import { useState, type ChangeEvent, useCallback } from "react";
 import { getCodeSandboxHost } from "@codesandbox/utils";
+import debounce from "lodash/debounce";
 
-type Hotel = { _id: string, chain_name: string; hotel_name: string; city: string, country: string };
+type Hotel = {
+  _id: string;
+  chain_name: string;
+  hotel_name: string;
+  city: string;
+  country: string;
+};
 
-type Country = { _id: string; country: string; countryisocode: string };
+type Country = {
+  _id: string;
+  country: string;
+  countryisocode: string;
+};
 
-type City = { _id: string; name: string };
+type City = {
+  _id: string;
+  name: string;
+};
 
-type ApiResponse = { hotels: Hotel[]; countries: Country[]; cities: City[] };
+type ApiResponse = {
+  hotels: Hotel[];
+  countries: Country[];
+  cities: City[];
+};
 
-const codeSandboxHost = getCodeSandboxHost(3001)
-const API_URL = codeSandboxHost ? `https://${codeSandboxHost}` : 'http://localhost:3001'
+const codeSandboxHost = getCodeSandboxHost(3001);
+const API_URL = codeSandboxHost
+  ? `https://${codeSandboxHost}`
+  : "http://localhost:3001";
 
 const fetchAndFilterData = async (value: string) => {
   const response = await fetch(
@@ -34,30 +54,39 @@ function App() {
   const [error, setError] = useState<string | null>(null);
   const [searchValue, setSearchValue] = useState("");
 
-  const fetchData = async (event: ChangeEvent<HTMLInputElement>) => {
+  const debouncedFetchData = useCallback(
+    debounce(async (query: string) => {
+      if (!query) {
+        setHotels([]);
+        setCountries([]);
+        setCities([]);
+        setShowClearBtn(false);
+        return;
+      }
+
+      setLoading(true);
+      setError(null);
+      try {
+        const { filteredHotels, filteredCountries, filteredCities } =
+          await fetchAndFilterData(query);
+        setHotels(filteredHotels);
+        setCountries(filteredCountries);
+        setCities(filteredCities);
+        setShowClearBtn(true);
+      } catch (err) {
+        console.log({ err });
+        setError("Error fetching data");
+      } finally {
+        setLoading(false);
+      }
+    }, 300),
+    []
+  );
+
+  const fetchData = (event: ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value.trim();
-    setSearchValue(event.target.value);
-
-    if (!query) {
-      setHotels([]);
-      setShowClearBtn(false);
-      return;
-    }
-
-    setLoading(true);
-    setError(null);
-    try {
-      const { filteredHotels, filteredCountries, filteredCities } =
-        await fetchAndFilterData(query);
-      setHotels(filteredHotels);
-      setCountries(filteredCountries);
-      setCities(filteredCities);
-      setShowClearBtn(true);
-    } catch (err) {
-      setError("Error fetching data");
-    } finally {
-      setLoading(false);
-    }
+    setSearchValue(query);
+    debouncedFetchData(query);
   };
 
   const clearSearch = () => {
