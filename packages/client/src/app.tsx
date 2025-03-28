2b0b39c22f1b927187f87bdf8543d9d9438de636
Sam De Maeyer <sam_demaeyer@hotmail.com>
2025-03-28 10:14:18 +0000

    Remove FE filter

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,36 +1,40 @@
-import { useState, type ChangeEvent } from 'react';
+import { useState, type ChangeEvent } from "react";
 import { getCodeSandboxHost } from "@codesandbox/utils";
 
-type Hotel = { _id: string, chain_name: string; hotel_name: string; city: string, country: string };
+type Hotel = {
+  _id: string;
+  chain_name: string;
+  hotel_name: string;
+  city: string;
+  country: string;
+};
 
-const codeSandboxHost = getCodeSandboxHost(3001)
-const API_URL = codeSandboxHost ? `https://${codeSandboxHost}` : 'http://localhost:3001'
+const codeSandboxHost = getCodeSandboxHost(3001);
+const API_URL = codeSandboxHost
+  ? `https://${codeSandboxHost}`
+  : "http://localhost:3001";
 
-const fetchAndFilterHotels = async (value: string) => {
+const fetchAndFilterHotels = async () => {
   const hotelsData = await fetch(`${API_URL}/hotels`);
-  const hotels = (await hotelsData.json()) as Hotel[];
-  return hotels.filter(
-    ({ chain_name, hotel_name, city, country }) =>
-      chain_name.toLowerCase().includes(value.toLowerCase()) ||
-      hotel_name.toLowerCase().includes(value.toLowerCase()) ||
-      city.toLowerCase().includes(value.toLowerCase()) ||
-      country.toLowerCase().includes(value.toLowerCase())
-  );
-}
+  return (await hotelsData.json()) as Hotel[];
+};
 
 function App() {
   const [hotels, setHotels] = useState<Hotel[]>([]);
   const [showClearBtn, setShowClearBtn] = useState(false);
 
   const fetchData = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value === '') {
+    if (event.target.value === "") {
       setHotels([]);
       setShowClearBtn(false);
       return;
     }
 
-    const filteredHotels = await fetchAndFilterHotels(event.target.value)
     setShowClearBtn(true);
+    const hotelsData = await fetchAndFilterHotels();
+    const filteredHotels = hotelsData.filter(({ hotel_name }) =>
+      hotel_name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
     setHotels(filteredHotels);
   };
 
@@ -57,15 +61,22 @@ function App() {
               {!!hotels.length && (
                 <div className="search-dropdown-menu dropdown-menu w-100 show p-2">
                   <h2>Hotels</h2>
-                  {hotels.length ? hotels.map((hotel, index) => (
-                    <li key={index}>
-                      <a href={`/hotels/${hotel._id}`} className="dropdown-item">
-                        <i className="fa fa-building mr-2"></i>
-                        {hotel.hotel_name}
-                      </a>
-                      <hr className="divider" />
-                    </li>
-                  )) : <p>No hotels matched</p>}
+                  {hotels.length ? (
+                    hotels.map((hotel, index) => (
+                      <li key={index}>
+                        <a
+                          href={`/hotels/${hotel._id}`}
+                          className="dropdown-item"
+                        >
+                          <i className="fa fa-building mr-2"></i>
+                          {hotel.hotel_name}
+                        </a>
+                        <hr className="divider" />
+                      </li>
+                    ))
+                  ) : (
+                    <p>No hotels matched</p>
+                  )}
                   <h2>Countries</h2>
                   <p>No countries matched</p>
                   <h2>Cities</h2>
