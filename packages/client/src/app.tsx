5a12cc0c790fa13bc3325fd7a6794d1aa95e644b
Sam De Maeyer <sam_demaeyer@hotmail.com>
2025-04-01 09:56:57 +0000

    Remove comveluted code

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,37 +1,30 @@
-import { useState, type ChangeEvent } from 'react';
+import { useState, type ChangeEvent } from "react";
 import { getCodeSandboxHost } from "@codesandbox/utils";
 
-type Hotel = { _id: string, chain_name: string; hotel_name: string; city: string, country: string };
+type Hotel = {
+  _id: string;
+  chain_name: string;
+  hotel_name: string;
+  city: string;
+  country: string;
+};
 
-const codeSandboxHost = getCodeSandboxHost(3001)
-const API_URL = codeSandboxHost ? `https://${codeSandboxHost}` : 'http://localhost:3001'
-
-const fetchAndFilterHotels = async (value: string) => {
-  const hotelsData = await fetch(`${API_URL}/hotels`);
-  const hotels = (await hotelsData.json()) as Hotel[];
-  return hotels.filter(
-    ({ chain_name, hotel_name, city, country }) =>
-      chain_name.toLowerCase().includes(value.toLowerCase()) ||
-      hotel_name.toLowerCase().includes(value.toLowerCase()) ||
-      city.toLowerCase().includes(value.toLowerCase()) ||
-      country.toLowerCase().includes(value.toLowerCase())
-  );
-}
+const codeSandboxHost = getCodeSandboxHost(3001);
+const API_URL = codeSandboxHost
+  ? `https://${codeSandboxHost}`
+  : "http://localhost:3001";
 
 function App() {
   const [hotels, setHotels] = useState<Hotel[]>([]);
-  const [showClearBtn, setShowClearBtn] = useState(false);
 
-  const fetchData = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value === '') {
+  const handleInput = async (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "") {
       setHotels([]);
-      setShowClearBtn(false);
       return;
     }
-
-    const filteredHotels = await fetchAndFilterHotels(event.target.value)
-    setShowClearBtn(true);
-    setHotels(filteredHotels);
+    const hotelsData = await fetch(`${API_URL}/hotels`);
+    const hotels = (await hotelsData.json()) as Hotel[];
+    setHotels(hotels);
   };
 
   return (
@@ -46,26 +39,31 @@ function App() {
                   type="text"
                   className="form-control form-input"
                   placeholder="Search accommodation..."
-                  onChange={fetchData}
+                  onChange={handleInput}
                 />
-                {showClearBtn && (
-                  <span className="left-pan">
-                    <i className="fa fa-close"></i>
-                  </span>
-                )}
+                <span className="left-pan">
+                  <i className="fa fa-close"></i>
+                </span>
               </div>
               {!!hotels.length && (
                 <div className="search-dropdown-menu dropdown-menu w-100 show p-2">
                   <h2>Hotels</h2>
-                  {hotels.length ? hotels.map((hotel, index) => (
-                    <li key={index}>
-                      <a href={`/hotels/${hotel._id}`} className="dropdown-item">
-                        <i className="fa fa-building mr-2"></i>
-                        {hotel.hotel_name}
-                      </a>
-                      <hr className="divider" />
-                    </li>
-                  )) : <p>No hotels matched</p>}
+                  {hotels.length ? (
+                    hotels.map((hotel, index) => (
+                      <li key={index}>
+                        <a
+                          href={`/hotels/${hotel._id}`}
+                          className="dropdown-item"
+                        >
+                          <i className="fa fa-building mr-2"></i>
+                          {hotel.hotel_name}
+                        </a>
+                        <hr className="divider" />
+                      </li>
+                    ))
+                  ) : (
+                    <p>No hotels matched</p>
+                  )}
                   <h2>Countries</h2>
                   <p>No countries matched</p>
                   <h2>Cities</h2>
